41ca7cada390a422142bd5be7d64718ea9f764aa
saaqibA21 <saaqibheroindia@gmail.com>
2026-04-04 00:50:58 +0530

    🚀 Final Play Store Release v1.0.0: Stable Android Build + Neural Semantic Engine + Deep Insight & TTS

---
+++ app/src/main/java/com/saaqiba21/thirukkural_ai/DelegationService.java
@@ -0,0 +1,14 @@
+package com.saaqiba21.thirukkural_ai;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
